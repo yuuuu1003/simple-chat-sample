4021c5f0c7c60db625696112277c44348e250379
Ko Fujimura <fujimura@otsuma.ac.jp>
2025-09-08 09:26:23 +0000

    system messages added

--- server.js
+++ server.js
@@ -8,10 +8,24 @@ const io = new Server(server);
 app.use(express.static('public'));
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  // Listen for a new user connecting
+  socket.on('user connected', (clientId) => {
+    socket.clientId = clientId;
+    console.log(clientId + ' connected');
+    // Welcome the new user
+    socket.emit('welcome', clientId);
+    // Notify all other clients that a new user has joined
+    socket.broadcast.emit('user joined', clientId);
   });
+
+  socket.on('disconnect', () => {
+    if (socket.clientId) {
+      console.log(socket.clientId + ' disconnected');
+      // Notify all clients that a user has left
+      io.emit('user left', socket.clientId);
+    }
+  });
+
   socket.on('chat message', (msg) => {
     io.emit('chat message', msg);
   });
