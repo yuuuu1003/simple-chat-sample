fbf25b11e863afa511fad093d70a71d0387f3f68
Ko Fujimura <fujimura@otsuma.ac.jp>
2025-09-08 05:31:32 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+server.listen(8080, () => {
+  console.log('listening on *:8080');
+});
